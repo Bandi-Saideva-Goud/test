b0e2fca06c379e3e2d921acfdaf134e586703669
Bandi-Saideva-Goud <saidevabandi@gmail.com>
2022-03-13 19:34:08 +0530

    pizza

--- hungry.py
+++ hungry.py
@@ -1,6 +1,7 @@
 hungry = input("Are you hungry?")
 if hungry == "yes":
     print ("eat somasa")
+    print("eat pizza")
 else:
     thirsty = input("enter yes if you are thristy?")
     if thirsty == "yes":
