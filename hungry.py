be101992e213b02b9aeaa06b25c1454910cac425
Bandi-Saideva-Goud <saidevabandi@gmail.com>
2022-03-13 18:55:41 +0530

    soda

--- hungry.py
+++ hungry.py
@@ -5,5 +5,6 @@ else:
     thirsty = input("enter yes if you are thristy?")
     if thirsty == "yes":
         print("drink water")
+        print("drink soda")
     else:
         print("do your homework!")
